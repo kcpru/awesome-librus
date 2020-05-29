01658e714456b9da6657cca01f069b81d22fc160
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-05-29 19:57:46 +0200

    Modified styles

--- src/js/fonts.js
+++ src/js/fonts.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 
 const addFonts = () => {
-  const font = `<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">`
+  const font = `<link href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@400;700&display=swap" rel="stylesheet">`
 
   $('head').append(font)
 }
