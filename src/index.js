53a75b928a005359f1f15b1f7b6eb37edc0bfdc6
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-05-31 00:42:45 +0200

    Added new theme

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ import './styles/main.scss'
 const toggle = new ToggleTheme({
   theme: [
     { name: 'Light', value: 'light' },
+    { name: 'Light blue', value: 'light-blue' },
+    { name: 'Light green', value: 'light-green' },
+    { name: 'Light pink', value: 'light-pink' },
     { name: 'Dark', value: 'dark' },
     { name: 'Dark blue', value: 'dark-blue' },
+    { name: 'Dark violet', value: 'dark-violet' },
     { name: 'Amoled', value: 'amoled' },
+    { name: 'Mono', value: 'mono' },
   ],
 })
 
