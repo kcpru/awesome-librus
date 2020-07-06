2aa8280e83e03cdba37ad25c80395aabb4538176
kcpru <kacperuczyk@gmail.com>
2020-07-07 00:37:52 +0200

    Add listener

--- src/content/js/awesome-librus.js
+++ src/content/js/awesome-librus.js
@@ -3,13 +3,19 @@ class AwesomeLibrus {
     this.body = document.querySelector('body')
     this.API = chrome || browser
     this.storage = this.API.storage.local
+
     this.init()
+    this.onChangeStorage()
   }
 
   init() {
     this.storage.get(['theme', 'borderRadius'], (items) => {
       this.setOptions(items.theme, items.borderRadius)
     })
+  }
+
+  onChangeStorage() {
+    this.storage.onChanged.addListener(() => this.init())
   }
 
   setOptions(className, borderRadius) {
