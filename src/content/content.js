607d43f3e3f914b9341f803c7fda78a5e5a4d286
kcpru <kacperuczyk@gmail.com>
2020-09-08 22:44:00 +0200

    Add redirect

--- src/content/content.js
+++ src/content/content.js
@@ -1,6 +1,7 @@
 import router from './js/router'
 import addFonts from './js/fonts'
 import AwesomeLibrus from './js/awesome-librus'
+import './js/redirect'
 
 import './styles/content.scss'
 
---
+++ src/content/js/redirect.js
@@ -0,0 +1,5 @@
+const alternateView =
+  'https://synergia.librus.pl/gateway/ms/studentdatapanel/ui/powiadomienia'
+
+if (location.href === 'https://synergia.librus.pl/uczen/index')
+  window.location.replace(alternateView)
